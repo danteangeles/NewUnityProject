26966eba7c3cd333ba91c9f6a9e7e69e3f37ed00
angeles <angeles@mobileit.co.jp>
2017-01-26 18:18:00 +0900

    [FirstProject] my first commit. put version control on my project.

---
+++ Assets/Scripts/Scene2.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Scene2 : MonoBehaviour {
+
+	[SerializeField]
+	Text myCounter, buttonA_Text, buttonB_Text, buttonC_Text, buttonGo_Text;
+
+	[SerializeField]
+	Button buttonA, buttonB, buttonC, buttonGo;
+
+	private int counterTotal, counterA, counterB, counterC, interval;
+
+	// Use this for initialization
+	void Start () {
+		counterTotal = 0;
+		counterA = 0;
+		counterB = 0;
+		counterC = 0;
+		interval = 1;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void OnClick_Button(Button button){
+		Debug.Log ("You have clicked the button!");
+
+		if (button.tag == "ButtonA"){
+			counterA = counterA + interval;
+			buttonA_Text.text = counterA.ToString();
+			if (counterA >= 3)
+			{
+				buttonA.GetComponent<Button>().interactable = false;
+			}
+		}
+
+
+		if (button.tag == "ButtonB"){
+			counterB = counterB + interval;
+			buttonB_Text.text = counterB.ToString();
+			if (counterB >= 4)
+			{
+				buttonB.GetComponent<Button>().interactable = false;
+			}
+		}
+
+		if (button.tag == "ButtonC"){
+			counterC = counterC + interval;
+			buttonC_Text.text = counterC.ToString(); 
+			if (counterC >= 2)
+			{
+				buttonC.GetComponent<Button>().interactable = false;
+			}
+		}
+
+		counterTotal = counterA + counterB + counterC;
+		myCounter.text = counterTotal.ToString();
+
+	}
+
+	public void OnLCick_Go(){
+		if (counterTotal >= 9)
+		{
+			SceneManager.LoadScene("Scene3");
+		}
+		else
+		{
+			SceneManager.LoadScene("Scene1");
+		}
+
+	}
+
+
+}
---
+++ Assets/Scripts/Scene3.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Scene3 : MonoBehaviour {
+
+	[SerializeField]
+	Text counterText;
+
+	[SerializeField]
+	GameObject stamp;
+
+	private int counter;
+
+	Vector3 mousePosition, targetPosition;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		mousePosition = Input.mousePosition;
+
+		targetPosition = Camera.main.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, 1f));
+
+		stamp.transform.position = targetPosition;
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			Debug.Log("Pressed left click.");
+
+			Instantiate(stamp,stamp.transform.position,stamp.transform.rotation);
+
+			counter++;
+			counterText.text = counter.ToString();
+
+		}
+		if (Input.GetMouseButtonDown(1))
+		{
+			Debug.Log("Pressed right click.");
+		}
+
+		if (Input.GetMouseButtonDown(2))
+		{
+			Debug.Log("Pressed middle click.");
+		}
+	}
+
+	public void backToPreviousScene(){
+		SceneManager.LoadScene("Scene2");
+	}
+
+}
